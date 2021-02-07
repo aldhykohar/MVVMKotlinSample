29cb7055889eb611b6ef2d9d0402a3742fcc2fe4
aldhykohar <aldhykohar@gmail.com>
2021-02-07 08:47:19 +0800

    add nav drawer home activity

--- app/src/main/java/com/stimednp/mvvmkotlinsample/ui/home/HomeActivity.kt
+++ app/src/main/java/com/stimednp/mvvmkotlinsample/ui/home/HomeActivity.kt
@@ -1,12 +1,35 @@
 package com.stimednp.mvvmkotlinsample.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
+import android.database.DatabaseUtils
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.setContentView
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.stimednp.mvvmkotlinsample.R
+import com.stimednp.mvvmkotlinsample.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, binding.drawerLayout)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            Navigation.findNavController(this, R.id.fragment),
+            binding.drawerLayout
+        )
     }
 }
